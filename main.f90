a09de88dcb5f07e00eb29e06ea199877e751ebcc
TWC1221 <tc1221@ic.ac.uk>
2025-10-20 08:39:53 +0100

    multigroup diffuion

--- main.f90
+++ main.f90
@@ -1,5 +1,10 @@
 program main
     use m_driver
     implicit none
-    call heterogeneous_driver_m2() !MMS_driver() !vacuum_BC_driver()
+    call multigroup_diffusion_iter() !iterative_power_driver() !heterogeneous_driver_m2() !MMS_driver() !vacuum_BC_driver()
 end program main
+
+!rm -rf build
+!mkdir build && cd build
+!cmake ..
+!make -j
--- src/m_multigroup_1D_diffusion.f90
+++ src/m_multigroup_1D_diffusion.f90
@@ -1,13 +1,13 @@
 module m_multigroup_1D_diffusion
 implicit none
 contains
-    subroutine build_matrix_multigroup(N, alpha, G, phi, dx, Dif, A_scatter, S0)
+    subroutine build_matrix_multigroup(N, alpha, G, phi, dx, Dif, A_scatter, S0, gg, Sigma_a)
         use m_constants
         use m_thomas_algorithm
         implicit none
-        integer, intent(in) :: N, G
+        integer, intent(in) :: N, G, gg
         real(8), intent(in) :: alpha
-        real(8), intent(inout) :: phi(N), dx(N-1), Dif(N), A_scatter(G), S0(N)
+        real(8), intent(inout) :: phi(N), dx(N-1), Dif, A_scatter(1:G,1:G), S0(N), Sigma_a
 
         real(8), dimension(N) :: a, b, c, d, x
         integer :: ii
@@ -20,18 +20,18 @@ contains
         do ii = 1, N
             if (ii == 1) then
                 a(ii) = 0.0d0
-                b(ii) = ((Dif(ii)+Dif(ii+1))/(dx(ii)*dx(ii))) + 1/dx(ii)*(1-alpha)/(1+alpha) + sum(A_scatter) 
-                c(ii) = -(Dif(ii)+Dif(ii+1))/(dx(ii)*(dx(ii)))
+                b(ii) = ((Dif+Dif))/(dx(ii)*dx(ii))) + 1/dx(ii)*(1-alpha)/(1+alpha) + sum(A_scatter(gg,1:G)) + Sigma_a
+                c(ii) = -(Dif+Dif)/(dx(ii)*(dx(ii)))
                 d(ii) = S0(ii)
             elseif (ii == N) then
-                a(ii) = -(Dif(ii-1)+Dif(ii))/(dx(ii-1)*dx(ii-1))
-                b(ii) = ((Dif(ii)+Dif(ii-1))/dx(ii-1))/(dx(ii-1)) + 1/dx(ii-1)*(1-alpha)/(1+alpha) + sum(A_scatter)
+                a(ii) = -(Dif+Dif)/(dx(ii-1)*dx(ii-1))
+                b(ii) = ((Dif+Dif)/dx(ii-1))/(dx(ii-1)) + 1/dx(ii-1)*(1-alpha)/(1+alpha) + sum(A_scatter(gg,1:G)) + Sigma_a
                 c(ii) = 0.0d0
                 d(ii) = S0(ii)
             else
-                a(ii) = -(Dif(ii-1)+Dif(ii))/(dx(ii-1)*(dx(ii)+dx(ii-1)))
-                b(ii) = ((Dif(ii)+Dif(ii-1))/dx(ii-1)+(Dif(ii)+Dif(ii+1))/dx(ii))/(dx(ii)+dx(ii-1)) + sum(A_scatter)
-                c(ii) = -(Dif(ii)+Dif(ii+1))/(dx(ii)*(dx(ii)+dx(ii-1)))
+                a(ii) = -(Dif+Dif)/(dx(ii-1)*(dx(ii)+dx(ii-1)))
+                b(ii) = ((Dif+Dif)/dx(ii-1)+(Dif+Dif)/dx(ii))/(dx(ii)+dx(ii-1)) + sum(A_scatter(gg,1:G)) + Sigma_a
+                c(ii) = -(Dif+Dif)/(dx(ii)*(dx(ii)+dx(ii-1)))
                 d(ii) = S0(ii)
             end if
         end do
