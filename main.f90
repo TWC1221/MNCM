569a87fe033a9dee2257ffc705fa868be79164fe
TWC1221 <tc1221@ic.ac.uk>
2025-11-11 13:15:41 +0000

    main

--- main.f90
+++ main.f90
@@ -1,9 +1,9 @@
 program main
-    use m_driver
-    use m_sparse_mm
+    use m_iter
     implicit none
-
-    call PCG_solver !multigroup_diffusion_iter() !CSR_rebuild !sparse_matrix_multiplication !iterative_power_driver() !heterogeneous_driver_m2() !MMS_driver() !vacuum_BC_driver()
+    integer :: PCG_mode = 3, max_iter = 100
+    call iter(PCG_mode, max_iter)
+    !call PCG_solver !multigroup_diffusion_iter() !CSR_rebuild !sparse_matrix_multiplication !iterative_power_driver() !heterogeneous_driver_m2() !MMS_driver() !vacuum_BC_driver()
 end program main
 
 !rm -rf build
