96c6d951f9ed620d3115650aa5549e853c8fbc84
TWC1221 <tc1221@ic.ac.uk>
2025-11-14 08:22:01 +0000

    main

--- src/m_2D_NDE/m_assembly.f90
+++ src/m_2D_NDE/m_assembly.f90
@@ -2,7 +2,7 @@ module m_diffusion_matrix
     implicit none
     contains
 
-    subroutine assemble_2D_diffusion_matrix(nx, ny, x_domain, y_domain, D, Sigma_a, ia, ja, a, dx, dy)
+  subroutine assemble_2D_diffusion_matrix(nx, ny, x_domain, y_domain, D, Sigma_a, ia, ja, a, dx, dy)
     implicit none
     !---------------------------------------------------------------
     ! Assembles CSR matrix for 2D diffusion operator:
@@ -96,12 +96,94 @@ module m_diffusion_matrix
 
     do ii = 1, N
         do ptr = ia(ii), ia(ii+1)-1
-            AA(ii, ja(ptr)) = a(ptr)
+          AA(ii, ja(ptr)) = a(ptr)
         end do
     end do
 
     print '(25F5.2)', ((AA(ii,jj), jj=1,N), ii=1,N)
 
-    end subroutine assemble_2D_diffusion_matrix
+  end subroutine assemble_2D_diffusion_matrix
+  
+  subroutine assemble_rth_diffusion_matrix(nr, nth, R_domain, D, Sigma_a, ia, ja, a)
+    implicit none
+    !---------------------------------------------------------------
+    ! Assembles CSR matrix for 2D diffusion operator:
+    !   -D*(d2/dr2 + 1/r d/dr + 1/r2 d2/dth2) + Sigma_a
+    !
+    ! Input:
+    !   nr, nth     - grid points (including boundaries)
+    !   dr, dth     - grid spacing
+    !   D          - diffusion coefficient
+    !   Sigma_a    - absorption coefficient
+    !
+    ! Output:
+    !   ia(:)      - kk pointer (CSR)
+    !   ja(:)      - column indices (CSR)
+    !   a(:)       - nonzero values (CSR)
+    !
+    ! Notes:
+    !   Lexicographic ordering: k = ii + (jj-1)*nx
+    !---------------------------------------------------------------
+    integer, intent(in) :: nr, nth
+    real(8), intent(in) :: R_domain, D, Sigma_a
+    integer, allocatable, intent(out) :: ia(:), ja(:)
+    real(8), allocatable, intent(out) :: a(:)
+
+    integer :: N, ii, jj, kk, ptr, est_nnz
+    real(8) :: aN, aE, aS, aW, a0
+
+    nnz = 1
+    ia(1) = 1
+
+    do ii = 1, nr
+      do jj = 1, nth
+        row = (ii-1)*nth + jj
+        b(row) = 0.0d0
+
+        ri = r(ii)
+
+        aN = D*(2*ri+dr)/(2*ri*dr*dr)
+        aS = D*(2*ri-dr)/(2*ri*dr*dr)
+        aE = D/(ri*ri*dth*dth)
+        aW = aE
+        a0 = -(aN+aS)-2.0d0*aE + Sigma_a
+
+        if (ii == 1) then
+          a0 = a0 + aS
+        else 
+          kk = (ii-2)*nth + jj
+          ja(nnz) = kk
+          a(nnz) = aS
+          nnz = nnz + 1
+        end if
+
+        if (ii == nr) then
+          a0 = a0 + aN
+        else
+          kk = ii*Nth + jj
+          ja(nnz) = kk
+          a(nnz) = aN
+          nnz = nnz + 1
+        end if
+
+        kk = (ii-1)*nth + merge(jj-1,Nth,j>1)
+        ja(nnz) = kk
+        a(nnz) = aW
+
+        kk = (ii-1)*nth + merge(j-1,1,j<nth)
+        ja(nnz) = kk
+        a(nnz) = aE
+        nnz = nnz + 1
+        
+        ja(nnz) = row
+        a(nnz) = a0
+        nnz = nnz + 1
+
+        ia(row+1) = nnz
+
+      end do 
+    end do 
+
+  end subroutine assemble_rth_diffusion_matrix     
 
 end module m_diffusion_matrix
